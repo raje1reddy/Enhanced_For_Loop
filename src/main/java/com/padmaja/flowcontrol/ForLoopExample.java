66d1510cc71f85a56241856f2dc5aaab2eae371a
raje reddy <raje1reddy@gmail.com>
2019-10-09 12:14:27 +0100

    code for enjanced forloop array   example

---
+++ src/main/java/com/padmaja/flowcontrol/ForLoopExample.java
@@ -0,0 +1,36 @@
+package com.padmaja.flowcontrol;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class ForLoopExample {
+    public  int tablesExample() {
+        ArrayList <String> mylist = new ArrayList<String>();
+        mylist.add("java");
+        mylist.add("loop");
+        for(String val: mylist)
+            System.out.println(val);
+        return 0;
+        }
+    public  int tablesExample1() {
+        ArrayList <String> exams = new ArrayList<String>();
+        exams.add("java");
+        exams.add("oracle");
+        ArrayList <String> levels = new ArrayList<String>();
+        levels.add("basic");
+        levels.add("advanced");
+        ArrayList <String> grade = new ArrayList<String>();
+        grade.add("pass");
+        grade.add("fail");
+    ArrayList <ArrayList<String>>  nestedArrayList = new ArrayList<ArrayList<String>>();
+    nestedArrayList.add(exams);
+    nestedArrayList.add(levels);
+    nestedArrayList.add(grade);
+    for(ArrayList<String> nestedListedElement : nestedArrayList)
+        for(String element: nestedListedElement)
+           System.out.println(element);
+    return 0;
+    }
+
+}
+
+
